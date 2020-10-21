8e419cd3ad5b8016ae553e7a21cb8f9aee310e6b
YavuzA <yvzabdurrahman@gmail.com>
2020-10-21 12:26:17 -0400

    Adds toy for loop

--- com.yavuz.git.first/src/com/yavuz/git/first/GitTest.java
+++ com.yavuz.git.first/src/com/yavuz/git/first/GitTest.java
@@ -5,6 +5,9 @@ public class GitTest {
 	public static void main(String[] args) {
 		
 		System.out.println("Git is Fun, they say,hmm");
+		for(int i=0; i<10;i++)
+			System.out.println("git");
+			
 
 
 	}
