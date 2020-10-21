ffe878ca2c3a7e875e2cf2bd823e90aff4bc9ea5
a1maxx <yvzabdurrahman@gmail.com>
2020-10-21 15:58:17 -0400

    Formats code a little

--- Project1/src/nRalgorithm/Bus.java
+++ Project1/src/nRalgorithm/Bus.java
@@ -1,8 +1,10 @@
 package nRalgorithm;
 
+import org.apache.commons.lang3.reflect.TypeUtilsTest.This;
 import org.apache.commons.math3.analysis.differentiation.DerivativeStructure;
 import org.apache.commons.math3.linear.Array2DRowRealMatrix;
 import org.apache.commons.math3.linear.RealMatrix;
+
 
 public class Bus {
 	
@@ -12,28 +14,38 @@ public class Bus {
 	double p, q;
 	public DerivativeStructure voltage;
 	public DerivativeStructure delta;
+	private int index;
 	int order = 2;
 	double initialValue_d = 0.0;
 	double initialValue_v = 1.0;
 	boolean slack = false;
 
+	
+//	Constructor of the bus objects 	
 	Bus(int type, double[][] admittance, double[][] theta, int index, double P, double Q) {
 		this.type = type;
 		// int params = admittance.length;
 		int params = 6;
 		this.admittance = new Array2DRowRealMatrix(admittance);
 		this.theta = new Array2DRowRealMatrix(theta);
+		this.index = index;
 		this.p = P;
 		this.q = Q;
+		
 		delta = new DerivativeStructure(params, order, index, initialValue_d);
 		voltage = new DerivativeStructure(params, order, index + 1, initialValue_v);
 
 	}
 
+	
+	
+//	Dummy Constructors
 	public Bus() {
 
 	}
 
+	
+//	Creation of the power flow equations for a given array containing Bus objects
 	static DerivativeStructure[] createEqauations(Bus[] buses) {
 		int l = 0;
 		DerivativeStructure equations[] = new DerivativeStructure[buses.length];
@@ -88,7 +100,11 @@ public class Bus {
 		return equations;
 	}
 
+	
+	
+//	Determination of the unknows
 	DerivativeStructure[] detectUnknowns(Bus[] buses) {
+
 		DerivativeStructure unknowns[] = new DerivativeStructure[this.admittance.getRowDimension()];
 		int params = unknowns.length;
 		int order = 2;
@@ -104,6 +120,21 @@ public class Bus {
 			}
 		}
 		return unknowns;
+	}
+
+
+		
+// toString method for class Bus	
+	public String toString() {
+		
+		StringBuilder sb =  new StringBuilder();
+		sb.append("This is Bus with type of ");
+		sb.append(this.type);
+		sb.append("\n Index of the bus is ");
+		sb.append(this.index);
+		return sb.toString();
+		
+		
 	}
 
 }
--- Project1/src/nRalgorithm/GenericNR.java
+++ Project1/src/nRalgorithm/GenericNR.java
@@ -3,6 +3,9 @@ package nRalgorithm;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
+
+import org.apache.commons.math3.analysis.differentiation.DerivativeStructure;
 
 public class GenericNR {
 	
@@ -10,12 +13,32 @@ public class GenericNR {
 		ArrayList<Double> d = new ArrayList<Double>();
 		d.add(1.0);
 		d.add(2.0);
-		System.out.println(d.get(0));
-		System.out.println(d.indexOf(1.0));
+		
 		ArrayList <String> s = new ArrayList<String>();
 		s.add("fp".concat(Integer.toString(1)));
-		System.out.print(s.indexOf("fp".concat(Integer.toString(1))));
-		System.out.print("\na");
+
+		HashMap<Integer,String> hm = new HashMap<Integer,String>();
+		hm.put(1, "fp1");
+		System.out.println(hm.get(1));
+		double[][] admittances = { { 11, 12, 13 }, { 21, 22, 23 }, { 31, 32, 33 } };
+		double[][] theta = { { 11, 12, 13 }, { 21, 22, 23 }, { 31, 32, 33 } };
+		int params = 6;
+		int order = 2;
+		Bus[] buses = new Bus[3];
+		buses[0] = new Bus(3, admittances, theta, 0, 0, 0);
+		buses[1] = new Bus(0, admittances, theta, 2, -0.9, -0.5);
+		buses[2] = new Bus(1, admittances, theta, 4, 0.6, 0);
+		buses[0].voltage = new DerivativeStructure(params, order, 1, 1.0);
+		buses[0].delta = new DerivativeStructure(params, order, 0, 0);
+		buses[2].voltage = new DerivativeStructure(params, order, 5, 1.01);
+		System.out.println(buses[0]);
+		
+		for(int i = 0;i< buses.length;i++) {
+			
+			
+		}
+		
+		
 		
 	}
 	
